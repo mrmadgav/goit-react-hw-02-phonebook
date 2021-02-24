e7464064a5a57de32a30805733d1647ccf80fdd1
madgav <madgav@mail.ru>
2021-02-24 19:59:14 +0600

    Update Phone.jsx

--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -51,7 +51,18 @@ class Phone extends Component {
       contacts: prevState.contacts.filter((item) => item.id !== contactId),
     }));
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     return (
       <div>
